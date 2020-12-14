77c086bbbd1bf05eba4431a2a4fb31b0c4ed58b0
toto <antoniolabate19@gmail.com>
2020-12-14 09:01:44 +0100

    added root access to docker commands

--- dockerMotionEye/dockerConfigMotioneyeos.sh
+++ dockerMotionEye/dockerConfigMotioneyeos.sh
@@ -8,12 +8,12 @@ curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker pi
 
 #PORTAINER INSTALLATION
-docker pull portainer/portainer:linux-arm
-docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer:linux-arm
+sudo docker pull portainer/portainer:linux-arm
+sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer:linux-arm
 
 #MOTIONEYEOS CONTAINER INSTALLATION
-docker pull ccrisan/motioneye:master-armhf
-docker run --name="motioneye" \
+sudo docker pull ccrisan/motioneye:master-armhf
+sudo docker run --name="motioneye" \
     -p 8765:8765 -p 8081:8081 \
     --hostname="motioneye" \
     -v /etc/localtime:/etc/localtime:ro \
