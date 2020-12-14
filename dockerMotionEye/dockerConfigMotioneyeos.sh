d88b7b62664bdf3846fd5711f1c8e0f80024ed15
toto <antoniolabate19@gmail.com>
2020-12-14 09:34:18 +0100

    added progression prompts

--- dockerMotionEye/dockerConfigMotioneyeos.sh
+++ dockerMotionEye/dockerConfigMotioneyeos.sh
@@ -2,15 +2,15 @@
 #All passages in the README
 #UPDATES
 sudo apt update && sudo apt upgrade
-#
+echo "Updates finished" && echo
 #DOCKER INSTALLATION
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker pi
-
+echo "Docker installation finished" && echo
 #PORTAINER INSTALLATION
 sudo docker pull portainer/portainer:linux-arm
 sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer:linux-arm
-
+echo "Portainer installation finished" && echo
 #MOTIONEYEOS CONTAINER INSTALLATION
 sudo docker pull ccrisan/motioneye:master-armhf
 sudo docker run --name="motioneye" \
@@ -22,3 +22,6 @@ sudo docker run --name="motioneye" \
     --restart="always" \
     --detach=true \
     ccrisan/motioneye:master-armhf
+
+echo "MotionEyeOs container deployment finished"
+echo "Installation finished. Check the setup your host machine's ip at port 9000."
